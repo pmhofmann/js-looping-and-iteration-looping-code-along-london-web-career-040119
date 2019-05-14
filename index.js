e4ad12933fecea9fc5cbcb26e60317fe49794578
pmhofmann <pm.hofmann@yahoo.co.uk>
2019-05-14 10:54:10 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+function writeCards(arr, message) {
+    let newArr = []
+    for(let ele of arr) {
+        newArr.push( `Thank you, ${ele}, for the wonderful ${message} gift!`)
+    }
+    return newArr
+}
+
+
+function countdown(countdown){
+    while (countdown > 0) {
+      console.log(countdown);
+      countdown -= 1;
+    }
+    console.log(countdown);
+  } 
